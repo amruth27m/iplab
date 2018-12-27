596b51a72e16835b8576aab0b00e1357286adccd
Amruth Lal <amruth27m@gmail.com>
2018-12-27 15:29:41 +0530

    Exercise 1 completed

---
+++ exercise-1/exercise-1/1.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('resources/sample.tif',cv2.IMREAD_GRAYSCALE)
+minimum_pixel_value, maximum_pixel_value = 255,0
+
+for x in range(0,img.shape[0]):
+    for y in range(0, img.shape[1]):
+        if img[x,y] <= minimum_pixel_value:
+            minimum_pixel_value = img[x,y]
+        if img[x,y] >= maximum_pixel_value:
+            maximum_pixel_value = img[x,y]
+print("Minimum pixel value is %d" %minimum_pixel_value)
+print("Maximum pixel value is %d" %maximum_pixel_value)
